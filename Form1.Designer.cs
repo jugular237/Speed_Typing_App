a42a35e3b8342121900e1ba007da8b641fc27b76
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2022-05-22 20:04:03 +0300

    change color textpanel

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,16 @@ namespace Speed_Typing_App
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
             this.TextPanel = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // printDocument1
@@ -42,12 +47,15 @@ namespace Speed_Typing_App
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(114, 151);
+            this.textBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(122, 133);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(576, 327);
+            this.textBox1.Size = new System.Drawing.Size(433, 266);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = " ";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // printPreviewDialog1
             // 
@@ -62,22 +70,62 @@ namespace Speed_Typing_App
             // 
             // TextPanel
             // 
-            this.TextPanel.Location = new System.Drawing.Point(238, 12);
+            this.TextPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextPanel.Location = new System.Drawing.Point(176, 11);
+            this.TextPanel.Margin = new System.Windows.Forms.Padding(2);
             this.TextPanel.Name = "TextPanel";
-            this.TextPanel.Size = new System.Drawing.Size(625, 121);
+            this.TextPanel.Size = new System.Drawing.Size(470, 99);
             this.TextPanel.TabIndex = 1;
             this.TextPanel.Text = "";
             this.TextPanel.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(260, 424);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Почати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(644, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(663, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(921, 536);
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(813, 487);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.TextPanel);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TypeRobot";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -90,6 +138,10 @@ namespace Speed_Typing_App
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
         private System.Windows.Forms.RichTextBox TextPanel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,12 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+
 
 namespace Speed_Typing_App
 {
-    
+
     public partial class Form1 : Form
     {
+        int ticks=6;
         public Form1()
         {
             InitializeComponent();
@@ -22,9 +25,11 @@ namespace Speed_Typing_App
         {
             TextToPrint textToPrnt = new TextToPrint();
             TextPanel.Text = textToPrnt.TextTPrint;
+            TextPanel.ReadOnly = true;
+            textBox1.ReadOnly = true;
         }
-        
 
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
         private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
             e.Graphics.DrawString(Text, new Font("Times new Roman", 46, FontStyle.Regular), Brushes.Black, new PointF(50, 50));
@@ -33,11 +38,77 @@ namespace Speed_Typing_App
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            ticks--;
+            timer1.Interval = 1000;
+            if (ticks < 0)
+            {
+                timer1.Interval = 1;
+                label1.Visible = false;
+                textBox1.ReadOnly = false;
+            }
+            else
+                label1.Text = ticks.ToString();
+            //ChangeClr(TextPanel);
+            ChangeClr1(TextPanel);
+
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextPanel.Font = new Font(TextPanel.Font, FontStyle.Regular);
+            timer1.Start();
+           
+        }
+        void ChangeClr(RichTextBox TextPanel)
+        {
+            int index = 0;
+            Input input = new Input();
+            TextToPrint textToPrnt = new TextToPrint();
+            input.Text = textBox1.Text;
+            foreach (var i in input.Text)
+            {
+                if (i == textToPrnt.TextTPrint[index])
+                {
+                    
+                    TextPanel.BackColor = Color.Green;
+                }
+                else TextPanel.BackColor = Color.Red;
+                index++;
+            }
+            //label2.Text= textToPrnt.TextTPrint[index].ToString();
+        }
+        void ChangeClr1(RichTextBox TextPanel)
+        {
+            Input input = new Input();
+            TextToPrint textToPrnt = new TextToPrint();
+            input.Text = textBox1.Text;
+            int lng = input.Text.Length;
+            string sub=textToPrnt.TextTPrint.Substring(0, lng);
+            if (sub == input.Text)
+            {
+                TextPanel.BackColor = Color.Green;
+            }
+            else TextPanel.BackColor = Color.Red;
+        }
+
+        public class TextToPrint
+        {
+            public string TextTPrint { get; set; } = "I love eat little childs and fuck kakashka";
+        }
+        public class Input
+        {
+            public string Text;
         }
     }
-    public class TextToPrint
-    {
-        public string TextTPrint { get; set; } = "Print this Text.";
-    }
 }
+    
