8618b781a9e4427be89edb6e761b3bc6887a29ff
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2022-05-24 14:14:52 +0300

    wpm

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,6 +42,8 @@ namespace Speed_Typing_App
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // printDocument1
@@ -50,35 +52,20 @@ namespace Speed_Typing_App
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(122, 183);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Multiline = true;
+            resources.ApplyResources(this.textBox1, "textBox1");
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(433, 216);
-            this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // printPreviewDialog1
             // 
-            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
-            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
-            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            resources.ApplyResources(this.printPreviewDialog1, "printPreviewDialog1");
             this.printPreviewDialog1.Document = this.printDocument1;
-            this.printPreviewDialog1.Enabled = true;
-            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
             this.printPreviewDialog1.Name = "printPreviewDialog1";
-            this.printPreviewDialog1.Visible = false;
             // 
             // TextPanel
             // 
-            this.TextPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.TextPanel.Location = new System.Drawing.Point(122, 11);
-            this.TextPanel.Margin = new System.Windows.Forms.Padding(2);
+            resources.ApplyResources(this.TextPanel, "TextPanel");
             this.TextPanel.Name = "TextPanel";
-            this.TextPanel.Size = new System.Drawing.Size(433, 148);
-            this.TextPanel.TabIndex = 1;
-            this.TextPanel.Text = "";
             this.TextPanel.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // timer1
@@ -87,32 +74,20 @@ namespace Speed_Typing_App
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(260, 424);
+            resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(179, 51);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Почати";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(569, 221);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 24);
-            this.label1.TabIndex = 3;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(569, 38);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 24);
-            this.label2.TabIndex = 4;
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // timer2
@@ -121,30 +96,34 @@ namespace Speed_Typing_App
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(560, 14);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(200, 24);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Пройшло часу(сек):";
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(560, 187);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(173, 24);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "До початку(сек):";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RosyBrown;
-            this.ClientSize = new System.Drawing.Size(813, 487);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -152,9 +131,8 @@ namespace Speed_Typing_App
             this.Controls.Add(this.button1);
             this.Controls.Add(this.TextPanel);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.HelpButton = true;
             this.Name = "Form1";
-            this.Text = "TypeRobot";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -174,6 +152,8 @@ namespace Speed_Typing_App
         private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Globalization;
+
 
 
 namespace Speed_Typing_App
@@ -15,13 +17,32 @@ namespace Speed_Typing_App
 
     public partial class Form1 : Form
     {
-        int ticks=6;
+        int ticks = 6;
         int ticks1 = 0;
+        bool flag = true;
+        int misc = 0;
         public Form1()
         {
-            InitializeComponent();
+                InitializeComponent();
         }
-
+       
+        public class TextToPrint
+        {
+            public string TextTPrint { get; set; } = "I like eat little childs and i love shasha mandrik";
+            public int SymbCount = 0;
+        }
+        public class Input
+        {
+            public string Text;
+            public double acc;
+            public double wordcount = 0;
+            public double time;
+            public double Time
+            {
+                get { return time; }
+                set { time = value; }
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             TextToPrint textToPrnt = new TextToPrint();
@@ -32,7 +53,6 @@ namespace Speed_Typing_App
             label3.Visible=false;
         }
 
-        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
         private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
             e.Graphics.DrawString(Text, new Font("Times new Roman", 46, FontStyle.Regular), Brushes.Black, new PointF(50, 50));
@@ -48,8 +68,17 @@ namespace Speed_Typing_App
         {
 
         }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
+            Input input = new Input();
             ticks--;
             timer1.Interval = 1000;
             if (ticks < 0)
@@ -63,76 +92,80 @@ namespace Speed_Typing_App
                 timer2.Start();
 
             }
-                label1.Text = ticks.ToString();
-            
-            //ChangeClr(TextPanel);
-            ChangeClr1(TextPanel);
-
+            label1.Text = ticks.ToString();
+            ChangeClr1(TextPanel,input);
+            CheckOnEnd(input);
         }
-       
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            TextToPrint textToPrnt = new TextToPrint();
+            ticks1++;
+            timer2.Interval = 1000;
+            label2.Text = ticks1.ToString();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            TextPanel.Font = new Font(TextPanel.Font, FontStyle.Regular);
             timer1.Start();
-           
+
         }
-        void ChangeClr1(RichTextBox TextPanel)
+        void ChangeClr1(RichTextBox TextPanel, Input input)
         {
-            Input input = new Input();
             TextToPrint textToPrnt = new TextToPrint();
             input.Text = textBox1.Text;
             int lng = input.Text.Length;
             string sub=textToPrnt.TextTPrint.Substring(0, lng);
             if (sub == input.Text)
-            { 
+            {
                 sub.ToCharArray();
-                foreach (char c in sub) 
+                foreach (char c in sub)
                 {
                     if (c == ' ')
                     {
                         input.wordcount++;
                     }
-                    if (c==' '&&c+1==' ')
-                    {
-                        
-                    }
                 }
-                TextPanel.BackColor = Color.Green;
+                //MessageBox.Show("end");
+                TextPanel.BackColor = Color.LightGreen;
             }
-            else TextPanel.BackColor = Color.Red;
-            //Print(input);
-        }
-        //void Print(Input input)
-        //{
-        //    label1.Text=input.wordcount.ToString();
-        //   label1.Visible=true;
-        //}
-
-        public class TextToPrint
-        {
-            public string TextTPrint { get; set; } = "I love eat little childs and fuck kakashka. ";
+            else if(sub!= input.Text)
+            {
+                misc++;
+                TextPanel.BackColor = Color.LightCoral;
+            }
             
         }
-        public class Input
+        void CheckOnEnd(Input input)
         {
-            public string Text;
-            public double acc;
-            public double wordcount = 0;
-            public int time;
-
+            input.Text = textBox1.Text;
+            TextToPrint textToPrnt = new TextToPrint();
+            if (input.Text.Length == textToPrnt.TextTPrint.Length && input.Text == textToPrnt.TextTPrint && flag == true)
+            {
+                flag = false;
+                timer1.Stop();
+                timer2.Stop();
+                input.Time = ticks1;
+                Print(input);
+            }
+        }
+        void Print(Input input)
+        {
+            label5.Visible = true;
+            label5.Text = misc.ToString();
+            TextToPrint textToPrnt = new TextToPrint();
+            double correlem = textToPrnt.TextTPrint.Length - misc;
+            input.acc = (correlem / textToPrnt.TextTPrint.Length) * 100.0;
+            double wpm = (((input.wordcount + 1)/input.Time)*60);
+            MessageBox.Show($"Швидкість,слів в хвилину(WPM):{wpm:f0}\nТочність(accuracy)={input.acc:f1}%");
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-
+            Application.Restart();
         }
 
-        private void timer2_Tick(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-            ticks1++;
-            timer2.Interval = 1000;
-            label2.Text = ticks1.ToString();
-            
+
         }
     }
 }
